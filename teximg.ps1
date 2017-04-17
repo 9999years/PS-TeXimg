4e9a7b6e8db967c710b7b3ddd6dcbb1132b4b482
Rebecca Turner <637275@gmail.com>
2017-04-17 01:33:18 -0400

    starting work from square 1

---
+++ teximg.ps1
@@ -0,0 +1,20 @@
+﻿function TeXImg {
+	[CmdletBinding()]
+	Param(
+		[Parameter(
+			Mandatory = $True,
+			Position = 0,
+			ValueFromPipeline = $True,
+			ValueFromRemainingArguments = $True,
+			HelpMessage = "The LaTeX formula to render"
+		)]
+		$Formula
+	)
+	Process {
+		Push-Location
+		$tmp_folder = "~\.latex_tmp"
+		New-Item $tmp_folder
+		(Get-Item $tmp_folder).Attributes = "Archive", "Hidden"
+		Pop-Location
+	}
+}
