8e969e1a831c021cad895e32fa7118919c81b3b1
Rebecca Turner <637275@gmail.com>
2017-04-17 01:45:59 -0400

    latex code in the ps code

--- teximg.ps1
+++ teximg.ps1
@@ -8,13 +8,51 @@
 			ValueFromRemainingArguments = $True,
 			HelpMessage = "The LaTeX formula to render"
 		)]
-		$Formula
+		[String]$Formula,
+		[String[]]$Packages,
+		[Switch]$NotMath
 	)
 	Process {
 		Push-Location
 		$tmp_folder = "~\.latex_tmp"
 		New-Item $tmp_folder
 		(Get-Item $tmp_folder).Attributes = "Archive", "Hidden"
+
+		$body = "\( \displaystyle
+			$Formula
+		\)"
+
+		If($NotMath) {
+		}
+
+		$code = "
+\documentclass{article}
+\usepackage[
+	paperwidth  = 20cm,
+	paperheight = 10cm,
+	top         = 0.5cm,
+	bottom      = 0.5cm,
+	margin      = 0.5cm,
+	scale       = 1,
+	offset      = 0in,
+	noheadfoot,
+	nomarginpar,
+	]{geometry}
+\usepackage{adjustbox}
+\usepackage{pbox}
+\usepackage{amsmath}
+\usepackage{amssymb}
+\usepackage{mathtools}
+$(ForEach($package in $Packages) { "\usepackage{$package}" })
+\pagestyle{empty}
+\parindent=0em
+\parskip=0em
+\begin{document}
+	\maxsizebox*{0.99\textwidth}{0.99\textheight}{%
+	\minsizebox*{0.99\textwidth}{0.99\textheight}{%
+	}}%
+\end{document}"
+
 		Pop-Location
 	}
 }
